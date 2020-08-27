c312d27ba4ee452be223468437ebea377ae3a6b1
Ekta Saini <34946048+ektasaini@users.noreply.github.com>
2020-08-27 16:31:26 +0530

    reduce functions

--- checker.cpp
+++ checker.cpp
@@ -4,21 +4,16 @@ const int bpmlimit[]={70,150};
 const int spolimit =90;
 const int respRatelimit[]={30,95};
 
-bool bpmCheck(float bpm){
-   return (bpm >= bpmlimit[0]  && bpm <= bpmlimit[1]) ;
+bool isVitalInRange(float value , float lower, float upper ){
+   return (value>=lower && value <=upper);
 }
-bool spoCheck(float spo){
-  return (spo >= spolimit);
-}
-bool resRatecheck(float respRate){
-  return (respRate >=respRatelimit[0]  && respRate <= respRatelimit[1]);
-}
-  
 bool vitalsAreOk(float bpm, float spo, float respRate) {
-   return ( bpmCheck(bpm) && spoCheck(spo) && resRatecheck(respRate));
+   return (isVitalsInRange (bpm , bpmlimit[0],bpmlimit[1] ) && isVitalsInRange(spo,spolimit,INT_MAX) && isVitalsInRange(respRate ,respRatelimit[0],respRatelimit[1]) );
 }
 
 int main() {
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
+   
+  
 }
